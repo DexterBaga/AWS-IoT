f7521a9ad82989b4fb1eff152788cf56d5c3d453
DexterBaga <dexter.baga@gmail.com>
2016-11-08 07:01:38 -0800

    set module to show updates to to shadow; demonstrate publishing to a topic from a thing

--- thingShadowReader.js
+++ thingShadowReader.js
@@ -5,27 +5,37 @@ var awsIot = require('aws-iot-device-sdk');
 var thing = awsIot.thingShadow({
    keyPath: './certs/private.pem.key',
   certPath: './certs/certificate.pem.crt',
-    caPath: './certs/root-CA.crt',
-  clientId: 'tobeiot',
+    caPath: './certs/root-ca.crt',
+  clientId: 'tobeiotreader',
     region: 'us-east-1'
 });
 
-var colorOptions = ['R','G','B'];
+var thingName = 'demo';
 
-thing
-  .on('connect', function() {
-    console.log('connected thingShadow reader');
-  });
-  
-  thing
-  .on('message', function(topic, payload) {
-    console.log('message', topic, payload.toString());
-  });
+thing.on('connect', function() {
+      console.log('connected thingShadow reader');
+      thing.register(thingName);
+});   
 
-thing.on('delta', function(thingName, stateObject) {
+setInterval(function() {
+  var data = thing.get(thingName);
+  console.log(JSON.stringify(data))
+},3000);
+
+
+thing.on('status',  
+  function(thingName, stat, clientToken, stateObject) {  
+    console.log('\n----------------------------\n '+  
+    JSON.stringify(stateObject,null,2)); 
+
+    //var data = JSON.parse(stateObject);
+    if(stateObject["state"]["reported"]["color"] === "G"){
       thing.publish('thing/signal/color',
-            JSON.stringify({
-               message: 'delta value ' +
-                  JSON.stringify(stateObject.state)
-            }));
-   });
+                JSON.stringify({
+                  message: 'delta value ' +
+                      JSON.stringify(stateObject.state)
+                })); 
+    }
+    
+  });  
+
